72892fe7a042a7a5ec51b0eb27d254e11a6bf6ab
Ok945 <omkendre5454@gmail.com>
2022-12-11 00:52:53 +0530

    a basic fully functional cards animation with a cool design in completed but there is a one problem the website is not completely reponsive:

--- 2/main.js
+++ 2/main.js
@@ -26,9 +26,6 @@ var firstCard = document.getElementById("first__card");
             }, 30);
         }
 
-
-
-
         function myFunction1() {
             setTimeout(function () {
                 firstCard.classList.remove("animation_end_up");
@@ -72,8 +69,6 @@ var firstCard = document.getElementById("first__card");
             }, 30);
         }
 
-
-
         function myFunction3() {
             setTimeout(() => {
                 firstCard.classList.remove("animation_end_up");
@@ -95,3 +90,105 @@ var firstCard = document.getElementById("first__card");
                 }, 500);
             }, 30)
         }
+
+
+        
+        var cardElement4 = document.getElementById("fourth__card");
+        var cardElement1 = document.getElementById("first__card");
+        var cardElement2 = document.getElementById("second__card");
+        var cardElement3 = document.getElementById("third__card");
+
+
+
+        cardElement1.addEventListener('click',() =>{
+            setTimeout(function () {
+                firstCard.classList.remove("animation_end_up");
+                secondCard.classList.remove("animation_end_up");
+                thirdCard.classList.remove("animation_end_up");
+                fourthCard.classList.remove("animation_end_up");
+
+                secondCard.classList.remove("animation_end_below")
+                thirdCard.classList.remove("animation_end_below")
+                secondCard.classList.add("animation_start_up");
+                firstCard.classList.add("animation_start_below");
+                setTimeout(function () {
+                    secondCard.classList.remove("animation_start_up");
+                    secondCard.classList.add("animation_end_below")
+                    firstCard.classList.remove("animation_start_below");
+                    firstCard.classList.add("animation_end_up")
+
+                }, 500);
+
+
+            }, 30);
+        
+        })
+
+        cardElement2.addEventListener('click' , () =>{
+            setTimeout(function () {
+                firstCard.classList.remove("animation_end_up");
+                secondCard.classList.remove("animation_end_up");
+                thirdCard.classList.remove("animation_end_up");
+                fourthCard.classList.remove("animation_end_up");
+                
+                // firstCard.classList.add("animation_start_up");
+                thirdCard.classList.add("animation_start_up");
+                secondCard.classList.add("animation_start_below");
+
+                setTimeout(function () {
+                    thirdCard.classList.remove("animation_start_up");
+                    thirdCard.classList.add("animation_end_below")
+                    secondCard.classList.remove("animation_start_below");
+                    secondCard.classList.add("animation_end_up")
+                }, 500);
+            }, 30);
+        
+        })
+
+        cardElement3.addEventListener("click" , () =>{
+
+            setTimeout(() => {
+
+                firstCard.classList.remove("animation_end_up");
+                secondCard.classList.remove("animation_end_up");
+                thirdCard.classList.remove("animation_end_up");
+                fourthCard.classList.remove("animation_end_up");
+
+
+                secondCard.classList.remove("animation_end_below")
+                thirdCard.classList.remove("animation_end_below")
+                secondCard.classList.add("animation_start_up");
+                thirdCard.classList.add("animation_start_below");
+                setTimeout(() => {
+                    secondCard.classList.remove("animation_start_up");
+                    secondCard.classList.add("animation_end_below");
+                    thirdCard.classList.remove("animation_start_below");
+                    thirdCard.classList.add("animation_end_up")
+
+                }, 500);
+            }, 30);
+
+        })
+
+        cardElement4.addEventListener('click', () =>{
+            setTimeout(() => {
+                firstCard.classList.remove("animation_end_up");
+                secondCard.classList.remove("animation_end_up")
+                thirdCard.classList.remove("animation_end_up");
+                fourthCard.classList.remove("animation_end_up");
+
+                secondCard.classList.remove("animation_end_below");
+                thirdCard.classList.remove("animation_end_below");
+                secondCard.classList.add("animation_start_up");
+                fourthCard.classList.add("animation_start_below");
+
+                setTimeout(() => {
+                    secondCard.classList.remove("animation_start_up");
+                    secondCard.classList.add("animation_end_below");
+                    fourthCard.classList.remove("animation_start_below");
+                    fourthCard.classList.add("animation_end_up")
+
+                }, 500);
+            }, 30);
+        
+        })
